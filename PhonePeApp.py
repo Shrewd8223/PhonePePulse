c2c1e5b5d3fbd77c9e3929789b6935c86880d584
Shrewd8223 <Shreedhar_gehlot@persistent.com>
2023-05-05 11:56:14 +0530

    df:

--- PhonePeApp.py
+++ PhonePeApp.py
@@ -19,7 +19,7 @@ st.set_page_config(page_title="PhonePe Pulse", page_icon=icon,
                    layout="wide", initial_sidebar_state="expanded")
 
 
-#!Logo PhonePe Pulse(nitish)
+#Logo PhonePe Pulse(nitish)
 st.markdown('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">', unsafe_allow_html=True)
 st.markdown("""<nav class="navbar navbar-dark justify-content-center" style="background-color: #2c1942; height:60px" >
         <a class="navbar-brand" href="#" style="margin: 0px;background-color:#2c1942;">
